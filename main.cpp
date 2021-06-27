c439cd65e0350f06506ecacd34ecb7775ff4b11e
hsahujnpr <86568854+hsahujnpr@users.noreply.github.com>
2021-06-27 23:18:27 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  bst
+//
+//  Created by Himansu Shekhar Sahu on 19/05/21.
+//
+
+#include <iostream>
+
+class bst {
+    char*        name;
+    static int   treeCount;
+    
+    class tNode {
+        int    key;
+        class tNode *left, *right;
+        
+    public:
+        tNode* insert(int key, tNode* root);
+        tNode* remove(int key, tNode* root);
+        
+    };
+    
+    tNode*       root;
+    
+    
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
